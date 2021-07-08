7e0e2c97229d3673dacd8183094ed14842b5e4dc
Brandon Rimes <Brandon.Rimes@yahoo.com>
2021-07-08 09:25:07 -0400

    click change fan speed

--- script.js
+++ script.js
@@ -1,56 +1,32 @@
 // word bubble hover events -----------------------------------------
-    // self ----------------------
-document.getElementById("self").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>It's me!</p>";
-});
-    // email ---------------------
-document.getElementById("email").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>Send me an email!</p>";
-});
-    // linkedin ------------------
-document.getElementById("linkedin").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>Connect with me on LinkedIn!</p>";
-});
-    // github --------------------
-document.getElementById("github").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>See my code on GitHub!</p>";
-});
-    // ceiling fan ---------------
-document.getElementById("fan").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>My biggest fan!</p>";
-});
-    // book js -------------------
-document.getElementById("book-js").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>I know JavaScript!</p>";
-});
-    // book py -------------------
-document.getElementById("book-py").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>I know Python!</p>";
-});
-    // book html -----------------
-document.getElementById("book-html").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>I know HTML!</p>";
-});
-    // book css ------------------
-document.getElementById("book-css").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>I know CSS!</p>";
-});
-    // book django ---------------
-document.getElementById("book-django").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>I know Django!</p>";
-});
-    // book vue ------------------
-document.getElementById("book-vue").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>I know Vue.js!</p>";
-});
-    // puppub --------------------
-document.getElementById("puppub").addEventListener("mouseover", function() {
-    word.innerHTML = "<p>PupPub!<br>A Django REST && Vue app<br>hosted on Heroku</p>";
-});
-    // when not hovering an object
+const objects = document.getElementsByClassName("objects");
+for (var i = 0; i < objects.length; i++) {
+    objects[i].addEventListener("mouseover", function() {
+        word.innerHTML = `<p>${this.title}</p>`;
+    });
+}
+
 const surfaces = document.getElementsByClassName("surfaces");
 for (var i = 0; i < surfaces.length; i++) {
     surfaces[i].addEventListener("mouseover", function() {
         word.innerHTML = "<p>Welcome!<br>Make yourself at home.</p>";
     });
 }
+
+// ceiling fan speed ------------------------------------------------
+let fanSetting = 1;
+let fanSpeed = ""
+document.getElementById("fan").addEventListener("click", function() {
+    if (fanSetting === 3) {
+        fanSetting = 1;
+        fanSpeed = "5s";
+    } else if (fanSetting === 2) {
+        fanSetting = 3;
+        fanSpeed = "0.25s"
+    } else if (fanSetting === 1) {
+        fanSetting = 2;
+        fanSpeed = "1s"
+    }
+    
+    document.getElementById("fan-blades").style.animationDuration = fanSpeed;
+})
